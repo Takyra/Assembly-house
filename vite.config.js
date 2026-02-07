070f30739eac55106a4e4ef03f75efe73cecf8f0
Azarov Gennady <SomeGenHome@gmail.com>
2026-02-07 18:04:22 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/assembly-house/',
+  base: process.env.NODE_ENV === 'production'
+    ? '/Assembly-house/'
+    : '/',
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
